c79e95e421528c716145b5ce5e467e69823411fd
Heriklys Andrade <heriklysalvaroaa@gmail.com>
2024-10-20 19:51:22 -0300

    Documenting the API response types.

--- TesteB3.Service/TesteB3.Api/Controllers/CalculatorController.cs
+++ TesteB3.Service/TesteB3.Api/Controllers/CalculatorController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using TesteB3.Domain.Interfaces;
+using TesteB3.Domain.ResponseModels;
 using TesteB3.Domain.ViewModels;
 
 namespace TesteB3.Api.Controllers
@@ -11,6 +12,8 @@ namespace TesteB3.Api.Controllers
         private readonly ICdbService cdbService = cdbService;
 
         [HttpPost("cdb")]
+        [ProducesResponseType<CdbResponseModel>(StatusCodes.Status200OK)]
+        [ProducesResponseType<string[]>(StatusCodes.Status400BadRequest)]
         public IActionResult ComputeCdb([FromBody] CdbViewModel cdbViewModel)
         {
             try
