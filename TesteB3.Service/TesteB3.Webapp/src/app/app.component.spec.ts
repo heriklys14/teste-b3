0eccdd76d1b767ddf6d2db0f0029dad51d6e84e8
Heriklys Andrade <heriklysalvaroaa@gmail.com>
2024-10-20 16:34:44 -0300

    Removing unnused code.

--- TesteB3.Service/TesteB3.Webapp/src/app/app.component.spec.ts
+++ TesteB3.Service/TesteB3.Webapp/src/app/app.component.spec.ts
@@ -13,17 +13,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have the 'teste-b3' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('teste-b3');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, teste-b3');
-  });
 });
