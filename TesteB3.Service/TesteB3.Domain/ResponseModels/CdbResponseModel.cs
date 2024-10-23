23e7bb93bc6803cb21b6344b3396f32ea4fb9966
Heriklys Andrade <heriklysalvaroaa@gmail.com>
2024-10-23 13:56:18 -0300

    Changing the rounding criteria.

--- TesteB3.Service/TesteB3.Domain/ResponseModels/CdbResponseModel.cs
+++ TesteB3.Service/TesteB3.Domain/ResponseModels/CdbResponseModel.cs
@@ -3,9 +3,9 @@
     public class CdbResponseModel(double grossValue, double netvalue)
     {
         private readonly double _grossValue = grossValue;
-        public double GrossValue { get => Math.Round(_grossValue, 2, MidpointRounding.ToZero); }
+        public double GrossValue { get => Math.Round(_grossValue, 2); }
 
         private readonly double _netValue = netvalue;
-        public double NetValue { get => Math.Round(_netValue, 2, MidpointRounding.ToZero); }
+        public double NetValue { get => Math.Round(_netValue, 2); }
     }
 }
--- TesteB3.Service/TesteB3.Tests/Domain/ResponseModels/CdbResponseModelTest.cs
+++ TesteB3.Service/TesteB3.Tests/Domain/ResponseModels/CdbResponseModelTest.cs
@@ -7,9 +7,10 @@ namespace TesteB3.Tests.Domain.ResponseModels
     {
         [Trait("Constructor", "Success test scenario.")]
         [Theory]
-        [InlineData(10.123456223, 8.987654223, 10.12, 8.98)]
-        [InlineData(33.753456223, 28.987654223, 33.75, 28.98)]
-        [InlineData(48.883456223, 48.987654223, 48.88, 48.98)]
+        [InlineData(10.123456223, 8.987254223, 10.12, 8.99)]
+        [InlineData(11.123456223, 10.984254223, 11.12, 10.98)]
+        [InlineData(33.753456223, 28.988654823, 33.75, 28.99)]
+        [InlineData(58.883456223, 48.987004923, 58.88, 48.99)]
         public void CdbResponseModel_Success(double grossValue, double netValue, double roundedGrossValue, double roundedNetValue)
         {
             //Arrange & Act
@@ -25,7 +26,8 @@ namespace TesteB3.Tests.Domain.ResponseModels
         [InlineData(10.123456223, 8.987654223, 10.123456223, 8.987654223)]
         [InlineData(33.753456223, 28.987654223, 33.7534, 28.987)]
         [InlineData(48.883456223, 48.987654223, 48.8, 48.9)]
-        [InlineData(48.889456223, 48.987654223, 48.89, 48.99)]
+        [InlineData(48.889456223, 48.987654223, 48.88, 48.98)]
+        [InlineData(55.884456223, 52.013554203, 55.89, 52.02)]
         public void CdbResponseModel_Fail(double grossValue, double netValue, double roundedGrossValue, double roundedNetValue)
         {
             //Arrange & Act
